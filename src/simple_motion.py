8ea62ec3d957bdeaf8a8fa61aa05ff56aa1b3ec9
GenyKozlov <98642132+GenyKozlov@users.noreply.github.com>
2022-09-24 13:55:15 +0500

    24.09.2022

--- src/simple_motion.py
+++ src/simple_motion.py
@@ -1,32 +1,70 @@
+from cmath import atan, cos, pi, sin, sqrt
 import time
+import math
+import logging
 
 import cflib.crtp
+from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 
 URI = 'radio://0/80/2M/E7E7E7E703'
+
+CX = 0.8
+CY = -0.4
+k = 0.01
+R = 0.3
+v = 0.1
+
+logging.basicConfig(level=logging.ERROR)
+
+position_estimate = [0, 0]
 
 def take_off(cf, position):
     take_off_time = 1.0
     sleep_time = 0.1
     steps = int(take_off_time / sleep_time)
-    vz = position[2] / take_off_time
+    vz = position / take_off_time
 
     print(vz)
 
     for i in range(steps):
+        print ("take_off" + str(i))
         cf.commander.send_velocity_world_setpoint(0, 0, vz, 0)
         time.sleep(sleep_time)
 
+def forward(cf, distance):
+    sleep_time = 0.1
+    vx = 0.1
+    steps = int(distance / vx / sleep_time)
+
+    for i in range(steps):
+        print ("forward" + str(i))
+        cf.commander.send_velocity_world_setpoint(vx, 0, 0, 0)
+        time.sleep(sleep_time)
+
+def forward_circle(cf):
+    steps = 1000
+    for i in range (steps):
+        print ("forward_circle" + str(i))
+        print(position_estimate)
+        px = position_estimate[0]
+        py = position_estimate[1]
+        d, phi = distance_to_centre (px, py)
+        angle = phase_angle (d, phi)
+        vx, vy = get_velocity(v, angle)
+        cf.commander.send_velocity_world_setpoint(vx, vy, 0, 0)
+        time.sleep(0.1)
 
 def land(cf, position):
-    landing_time = 1.0
+    landing_time = 5.0
     sleep_time = 0.1
     steps = int(landing_time / sleep_time)
-    vz = -position[2] / landing_time
+    vz = -position / landing_time
 
     print(vz)
 
-    for _ in range(steps):
+    for i in range(steps):
+        print ("land" + str(i))
         cf.commander.send_velocity_world_setpoint(0, 0, vz, 0)
         time.sleep(sleep_time)
 
@@ -35,10 +73,48 @@ def land(cf, position):
     # since the message queue is not flushed before closing
     time.sleep(0.1)
 
+def distance_to_centre (px, py):
+    d = sqrt((px - CX)**2 + (py - CY)**2)
+    phi = math.atan2(px - CX, py - CY)
+    print('d= ' + str(d))
+    print('phi= ' + str(phi))
+    return (d, phi)
+
+def phase_angle (d, phi):
+    angle = phi + pi/2 + atan(k * (d - R))
+    print('angle= ' + str(angle))
+    return angle
+
+def get_velocity(v, angle):
+    vx = v * sin(angle)
+    vy = v * cos(angle)
+    print('vx= ' + str(vx))
+    print('vy= ' + str(vy))
+    return (vx.real, vy.real)
+
+def log_pos_callback(timestamp, data, logconf):
+    # print(data)
+    global position_estimate
+    position_estimate[0] = data['stateEstimate.x']
+    position_estimate[1] = data['stateEstimate.y']
+
+
 if __name__ == '__main__':
     cflib.crtp.init_drivers()
 
     with SyncCrazyflie(URI, cf=cflib.crazyflie.Crazyflie(rw_cache='./cache')) as scf:
-        take_off(scf, 0.5)
-        time.sleep(5)
-        land(scf, 0.05)
+
+        logconf = LogConfig(name='Position', period_in_ms=10)
+        logconf.add_variable('stateEstimate.x', 'float')
+        logconf.add_variable('stateEstimate.y', 'float')
+        scf.cf.log.add_config(logconf)
+        logconf.data_received_cb.add_callback(log_pos_callback)
+
+        logconf.start()
+
+        cf=scf.cf
+        take_off(cf, 0.3)
+        forward_circle(cf)
+        land(cf, 0)
+
+        logconf.stop()
