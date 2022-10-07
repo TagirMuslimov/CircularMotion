a4d3a39633f9618182a0143a2616444ac0a00eb8
Tagir Muslimov <tagir.muslimov@gmail.comgit config --global user.name Tagir>
2022-10-07 15:20:15 +0500

    one copter again

--- scripts/2Dplot.py
+++ scripts/2Dplot.py
@@ -5,7 +5,7 @@ plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
 
 headers = ['i', 'T_Z', 'v_z', 'CX', 'CY', 'k', 'R', 'D_12', 'v_f', 'v_cruis', 'k_f', 'p12', 'px_1', 'py_1', 'pz_1', 'd_1', 'phi_1', 'angle_1', 'v1', 'vx1', 'vy1', 'setPx1', 'setPy1', 'px_2', 'py_2', 'pz_2', 'd_2', 'phi_2', 'angle_2', 'v2', 'vx2', 'vy2', 'setPx2', 'setPy2']
-df = pd.read_csv('20221007-133730_setpos2.csv', names=headers, header=0, delimiter=';')
+df = pd.read_csv('20221007-145319_setpos2.csv', names=headers, header=0, delimiter=';')
 
 cols = df.columns
 for col in cols:
--- scripts/3Dplot.py
+++ scripts/3Dplot.py
@@ -5,9 +5,9 @@ plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
 ax = plt.axes(projection='3d')
 
-headers = ['timestamp', 'range.front', 'range.left', 'range.zrange']
+headers = ['i', 'T_Z', 'v_z', 'CX', 'CY', 'k', 'R', 'D_12', 'v_f', 'v_cruis', 'k_f', 'p12', 'px_1', 'py_1', 'pz_1', 'd_1', 'phi_1', 'angle_1', 'v1', 'vx1', 'vy1', 'setPx1', 'setPy1', 'px_2', 'py_2', 'pz_2', 'd_2', 'phi_2', 'angle_2', 'v2', 'vx2', 'vy2', 'setPx2', 'setPy2']
 
-df = pd.read_csv('D:/range2.csv', 
+df = pd.read_csv('20221007-145319_setpos2.csv', 
             names=headers, header=0, delimiter=';',
             skip_blank_lines=True,  engine='python'         
             )
@@ -15,12 +15,12 @@ df = pd.read_csv('D:/range2.csv',
 cols = df.columns
 for col in cols:
     df[col] = df[col].astype(float)
-df.set_index('timestamp').plot()
+df.set_index('i').plot()
 
 
-x = df['range.front']
-y = df['range.left']
-z = df['range.zrange']
+x = df['px_1']
+y = df['py_1']
+z = df['pz_1']
 
 plt.cla()
 
