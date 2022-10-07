e32320a78869cefdb0977fb8a63d5a402ec05e3d
GenyKozlov <98642132+GenyKozlov@users.noreply.github.com>
2022-10-07 14:05:36 +0500

    07.10.2022

--- scripts/2Dplot.py
+++ scripts/2Dplot.py
@@ -4,14 +4,13 @@ import matplotlib.pyplot as plt
 plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
 
-headers = ['i', 'x', 'y', 'd', 'phi', 'angle', 'vx', 'vy', 'CX', 'CY', 'k', 'R', 'v']
-
-df = pd.read_csv('1.csv', names=headers, header=0, delimiter=';')
+headers = ['i', 'T_Z', 'v_z', 'CX', 'CY', 'k', 'R', 'D_12', 'v_f', 'v_cruis', 'k_f', 'p12', 'px_1', 'py_1', 'pz_1', 'd_1', 'phi_1', 'angle_1', 'v1', 'vx1', 'vy1', 'setPx1', 'setPy1', 'px_2', 'py_2', 'pz_2', 'd_2', 'phi_2', 'angle_2', 'v2', 'vx2', 'vy2', 'setPx2', 'setPy2']
+df = pd.read_csv('20221007-133730_setpos2.csv', names=headers, header=0, delimiter=';')
 
 cols = df.columns
 for col in cols:
     df[col] = df[col].astype(float)
 
-df.plot(x='i', y=['x', 'y'])
+df.plot(x='px_2', y='py_2')
 
 plt.show()
