476e66a23c9e745b1117bb53705b7778d1ac0287
Tagir Muslimov <tagir.muslimov@gmail.comgit config --global user.name Tagir>
2022-10-08 17:00:35 +0500

    2 copters flight with battery check

--- scripts/2Dplot.py
+++ scripts/2Dplot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 plt.rcParams["figure.autolayout"] = True
 
-df = pd.read_csv('/home/tagir/CircularMotion/20221008-150520_setpos2.csv', header=0, delimiter=';')
+df = pd.read_csv('/home/tagir/CircularMotion/20221008-163951_setpos2.csv', header=0, delimiter=';')
 
 cols = df.columns
 for col in cols:
@@ -12,7 +12,7 @@ for col in cols:
 print(df.columns)
 
 # df.plot(x='i', y=['px_1','px_2','py_1','py_2'])
-df.plot(x='i', y=['d_1'])
+df.plot(x='i', y=['px_1'])
 
 
 plt.show()
