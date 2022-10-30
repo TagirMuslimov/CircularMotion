224a9557918e61385523780ccc51170c8edd86e7
TagirMuslimov <81864311+TagirMuslimov@users.noreply.github.com>
2022-10-30 20:49:07 +0500

    sorting data by folders

--- scripts/2Dplot.py
+++ scripts/2Dplot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 plt.rcParams["figure.autolayout"] = True
 
-df = pd.read_csv('/home/tagir/CircularMotion/20221015-154709_setpos.csv', header=0, delimiter=';')
+df = pd.read_csv(r'C:\Users\tagir\Documents\![Crazyflie Flight Tests]\![From Github]\CircularMotion\data\20221015-154359_setpos.csv', header=0, delimiter=';')
 
 cols = df.columns
 for col in cols:
@@ -13,9 +13,9 @@ print(df.columns)
 
 # df.plot(x='i', y=['setPx1','setPx2','setPx3'])
 # df.plot(x='i', y=['setPy1','setPy2','setPy3'])
-df.plot(x='i', y=['p12','p23'])
+df.plot(x='i', y=['p12', 'p23'])
 df.plot(x='i', y=['d_1','d_2', 'd_3'])
 df.plot(x='setPx1', y=['setPy1'])
 df.plot(x='setPx2', y=['setPy2'])
-df.plot(x='setPx3', y=['setPy3'])
+# df.plot(x='setPx3', y=['setPy3'])
 plt.show()
